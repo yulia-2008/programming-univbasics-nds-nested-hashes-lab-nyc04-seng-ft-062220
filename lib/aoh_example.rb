9edd0cc59617d9953cec2ac21920bf3888675f4d
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 16:47:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  []
+  [{ name:  "Don Gately", occupation: "Live-in Staff" }, ]
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants

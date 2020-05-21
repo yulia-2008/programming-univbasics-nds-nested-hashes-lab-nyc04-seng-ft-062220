6ac36f969b7406689ca3f80e1e671e5ac672c2e3
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 16:49:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,16 +14,18 @@ def assembled_aoh
 end
 
 def literal_aoh
-  [{ name:  "Don Gately", occupation: "Live-in Staff" },{ name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" },]
+  [{ name:  "Don Gately", occupation: "Live-in Staff" },{ name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" },{ name:  "Bruce Green", occupation: "Fan of Mildred" }]
   
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[row][key]=new_value
+  aoh
+  
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
